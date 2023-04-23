8db61e4acbb3ed70b767c148c47e1494bacfb534
tk <462304@qq.com>
2023-04-23 21:50:53 +0800

    增加lora merge权重
    Signed-off-by: tk <462304@qq.com>

--- infer_lora_finetuning.py
+++ infer_lora_finetuning.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2023/3/9 15:29
+import os
+
 from deep_training.data_helper import ModelArguments, TrainingArguments, DataArguments
 from deep_training.nlp.models.moss import MossConfig
 from deep_training.nlp.models.lora.v2 import LoraArguments
@@ -21,36 +23,34 @@ if __name__ == '__main__':
     tokenizer, _, _, _ = dataHelper.load_tokenizer_and_config(
         tokenizer_class_name=MossTokenizer, config_class_name=MossConfig)
 
-    config = MossConfig.from_pretrained('./best_ckpt')
+    ckpt_dir = './best_ckpt'
+    config = MossConfig.from_pretrained(ckpt_dir)
     config.initializer_weight = False
-
-    lora_args = LoraArguments.from_pretrained('./best_ckpt')
-
+    lora_args = LoraArguments.from_pretrained(ckpt_dir)
     assert lora_args.inference_mode == True
 
     pl_model = MyTransformer(config=config, model_args=model_args, training_args=training_args,lora_args=lora_args)
     # 加载lora权重
-    pl_model.backbone.from_pretrained(pl_model.backbone.model, pretrained_model_name_or_path = './best_ckpt', lora_config = lora_args)
+    pl_model.backbone.from_pretrained(pl_model.backbone.model, pretrained_model_name_or_path=ckpt_dir, lora_config = lora_args)
+    pl_model.eval().half().cuda()
 
-    model = pl_model.get_llm_model()
-    # 按需修改
-    model.half().cuda()
-    model = model.eval()
+    enable_merge_weight = False
+    if enable_merge_weight:
 
-    query = "<|Human|>: 你好<eoh>\n<|MOSS|>:"
-    response = model.chat(tokenizer, query,  max_length=2048,
-                          eos_token_id=config.eos_token_id,
-                          do_sample=True, top_p=0.7, temperature=0.95,
-                          )
-    print(query, ' 返回: ', response)
+        # 合并lora 权重 保存
+        pl_model.save_pretrained_merge_lora(os.path.join(ckpt_dir, 'pytorch_model_merge.bin'))
+    else:
+        model = pl_model.get_llm_model()
+        query = "<|Human|>: 你好<eoh>\n<|MOSS|>:"
+        response = model.chat(tokenizer, query,  max_length=2048,
+                              eos_token_id=config.eos_token_id,
+                              do_sample=True, top_p=0.7, temperature=0.95,
+                              )
+        print(query, ' 返回: ', response)
 
-    query = response + "\n<|Human|>: 推荐五部科幻电影<eoh>\n<|MOSS|>:"
-    response = model.chat(tokenizer, query, max_length=2048,
-                          eos_token_id=config.eos_token_id,
-                          do_sample=True, top_p=0.7, temperature=0.95,
-                          )
-    print(query, ' 返回: ', response)
-
-
-
-
+        query = response + "\n<|Human|>: 推荐五部科幻电影<eoh>\n<|MOSS|>:"
+        response = model.chat(tokenizer, query, max_length=2048,
+                              eos_token_id=config.eos_token_id,
+                              do_sample=True, top_p=0.7, temperature=0.95,
+                              )
+        print(query, ' 返回: ', response)

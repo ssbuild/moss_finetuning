24f28184f14af881a8dcf5529dd9663095c5c301
ssbuild <462304@qq.com>
2023-05-21 17:15:09 +0800

    remove  load_in_8bit infer_lora_finetuning.py
    Signed-off-by: ssbuild <462304@qq.com>

--- infer_lora_finetuning.py
+++ infer_lora_finetuning.py
@@ -27,7 +27,10 @@ if __name__ == '__main__':
     assert lora_args.inference_mode == True
 
     pl_model = MyTransformer(config=config, model_args=model_args, training_args=training_args,lora_args=lora_args,
-                             load_in_8bit=load_in_8bit, device_map="auto")
+                             # load_in_8bit=global_args["load_in_8bit"],
+                             # # device_map="auto",
+                             # device_map = {"":0} # 第一块卡
+                             )
     # 加载lora权重
     pl_model.backbone.from_pretrained(pl_model.backbone.model, pretrained_model_name_or_path=ckpt_dir, lora_config =lora_args)
     if load_in_8bit:
